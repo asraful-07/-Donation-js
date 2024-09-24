6b680a5c0e58103c7dcc8e41f486207058c82bb9
asraful-07 <asraful0508@gmail.com>
2024-09-24 13:17:48 +0600

    all js file successfully modle done

--- js/index.js
+++ js/index.js
@@ -1,83 +1,69 @@
-document.getElementById('donate-now').addEventListener('click', function() {
-    const accountBalance = getElementTextById('account-balance');
-    const donationInput = getInputValueById('donation-input');
-    const currentDonation = getElementTextById('current-donation');
-    // Clear input field after donation
-    document.getElementById('donation-input').value = "";
-  
-    if (isNaN(donationInput) || donationInput <= 0) {
-        alert('Please enter a valid donation amount');
-        return;
-    }
+document.getElementById("donate-now").addEventListener("click", function () {
+  const accountBalance = getElementTextById("account-balance");
+  const donationInput = getInputValueById("donation-input");
+  const currentDonation = getElementTextById("current-donation");
+  // Clear input field after donation
+  document.getElementById("donation-input").value = "";
 
-    if (donationInput > accountBalance) {
-        alert('Insufficient funds for this donation');
-        return;
-    }
+  if (isNaN(donationInput) || donationInput <= 0) {
+    return alert("Please enter a valid donation amount");
+  }
 
-    const newBalance = accountBalance - donationInput;
-    const newDonationTotal = currentDonation + donationInput;
-    document.getElementById('account-balance').textContent = newBalance.toFixed(2);
-    document.getElementById('current-donation').innerText = newDonationTotal.toFixed(2);
+  if (donationInput > accountBalance) {
+    return alert("Insufficient funds for this donation");
+  }
 
-    // History add
-    const now = new Date(); 
-     // Format the date to include the day of the week and the date
-     const formattedDate = now.toLocaleDateString('en-BD', {
-        weekday: 'long',   
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    });
+  const newBalance = accountBalance - donationInput;
+  const newDonationTotal = currentDonation + donationInput;
+  document.getElementById("account-balance").textContent = newBalance.toFixed(2);
+  document.getElementById("current-donation").innerText = newDonationTotal.toFixed(2);
 
- // Format the time to include hours, minutes, seconds, and time zone
-    const formattedTime = now.toLocaleTimeString('en-BD', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZoneName: 'short'  
-    });
+  // History add
+  const now = new Date();
+  // Format the date to include the day of the week and the date
+  const formattedDate = now.toLocaleDateString("en-BD", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-    const historyList = document.createElement('div');
-    historyList.className =
-    "bg-white shadow-xl rounded border-2 p-6 mb-6";
-    historyList.innerHTML = `
+  // Format the time to include hours, minutes, seconds, and time zone
+  const formattedTime = now.toLocaleTimeString("en-BD", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  });
+
+  const historyList = document.createElement("div");
+  historyList.className = "bg-white shadow-xl rounded border-2 p-6 mb-6";
+  historyList.innerHTML = `
     <h3 class="text-xl font-bold my-4">${donationInput} Taka is Donated for famine-2024 at Noakhali, Banglades</h3>
     <p class="text-lg text-black">Date: ${formattedDate} ${formattedTime} (Bangladesh Standard Time)</p>
    
-    `
-    document.getElementById('history-list').appendChild(historyList);
+    `;
+  document.getElementById("history-list").appendChild(historyList);
+
+  // Show Modal
+  document.getElementById("success-modal").showModal();
 });
+
 
 // donation to History tab
 const showHistory = document.getElementById("show-history");
 const showDonation = document.getElementById("show-donation");
 
-showHistory.addEventListener('click', function(){
-    showHistory.classList.add(
-        "text-black",
-        "bg-[#b4f461]"  
-    );
-    showDonation.classList.remove(
-        "text-black",
-        "bg-[#b4f461]"
-    );
-    document.getElementById("donation-section").classList.add("hidden");
-    document.getElementById("history-section").classList.remove("hidden");
+showHistory.addEventListener("click", function () {
+  showHistory.classList.add("text-black", "bg-[#b4f461]");
+  showDonation.classList.remove("text-black", "bg-[#b4f461]");
+  document.getElementById("donation-section").classList.add("hidden");
+  document.getElementById("history-section").classList.remove("hidden");
 });
 
-showDonation.addEventListener('click', function(){
-    showDonation.classList.add(
-        "text-black",
-        "bg-[#b4f461]",
-        "border-2"
-    );
-    showHistory.classList.remove(
-        "text-black",
-        "bg-[#b4f461]"
-
-    );
-    document.getElementById("donation-section").classList.remove("hidden");
-    document.getElementById("history-section").classList.add("hidden");
+showDonation.addEventListener("click", function () {
+  showDonation.classList.add("text-black", "bg-[#b4f461]", "border-2");
+  showHistory.classList.remove("text-black", "bg-[#b4f461]");
+  document.getElementById("donation-section").classList.remove("hidden");
+  document.getElementById("history-section").classList.add("hidden");
 });
-
--- js/index3.js
+++ js/index3.js
@@ -1,49 +1,52 @@
-document.getElementById('donation-btn').addEventListener('click', function(){
-    const accountBalance = getElementTextById('account-balance');
-    const donatedInputType = getInputValueById('donation-input-type');
-    const accountCash = getElementTextById('account-Amount');
-    // Clear input field after donation
-    document.getElementById('donation-input-type').value = "";
-  
-    if (isNaN(donatedInputType) || donatedInputType <= 0) {
-        alert('Please enter a valid donation amount');
-        return;
-    }
+document.getElementById("donation-btn").addEventListener("click", function () {
+  const accountBalance = getElementTextById("account-balance");
+  const donatedInputType = getInputValueById("donation-input-type");
+  const accountCash = getElementTextById("account-Amount");
+  // Clear input field after donation
+  document.getElementById("donation-input-type").value = "";
 
-    if (donatedInputType > accountBalance) {
-        alert('Insufficient funds for this donation');
-        return;
-    }
+  if (isNaN(donatedInputType) || donatedInputType <= 0) {
+    alert("Please enter a valid donation amount");
+    return;
+  }
 
-    const newCash = accountBalance - donatedInputType;
-    const newDonationTotalAmount = accountCash + donatedInputType;
-    document.getElementById('account-balance').textContent = newCash.toFixed(2);
-    document.getElementById('account-Amount').innerText = newDonationTotalAmount.toFixed(2);
+  if (donatedInputType > accountBalance) {
+    alert("Insufficient funds for this donation");
+    return;
+  }
 
-    // history add 
-    const now = new Date(); 
-     // Format the date to include the day of the week and the date
-     const formattedDate = now.toLocaleDateString('en-BD', {
-        weekday: 'long',   
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    });
+  const newCash = accountBalance - donatedInputType;
+  const newDonationTotalAmount = accountCash + donatedInputType;
+  document.getElementById("account-balance").textContent = newCash.toFixed(2);
+  document.getElementById("account-Amount").innerText =
+    newDonationTotalAmount.toFixed(2);
 
- // Format the time to include hours, minutes, seconds, and time zone
-    const formattedTime = now.toLocaleTimeString('en-BD', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZoneName: 'short'  
-    });
+  // history add
+  const now = new Date();
+  // Format the date to include the day of the week and the date
+  const formattedDate = now.toLocaleDateString("en-BD", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-    const historyList = document.createElement('div');
-    historyList.className =
-    "bg-white shadow-xl rounded border-2 p-6 mb-6";
-    historyList.innerHTML = `
+  // Format the time to include hours, minutes, seconds, and time zone
+  const formattedTime = now.toLocaleTimeString("en-BD", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+  });
+
+  const historyList = document.createElement("div");
+  historyList.className = "bg-white shadow-xl rounded border-2 p-6 mb-6";
+  historyList.innerHTML = `
     <h3 class="text-xl font-bold my-4">${donatedInputType} Taka is Donated for famine-2024 at Quota Movement, Banglades</h3>
     <p class="text-lg text-black">Date: ${formattedDate} ${formattedTime} (Bangladesh Standard Time)</p>
-    `
-    document.getElementById('history-list').appendChild(historyList);
+    `;
+  document.getElementById("history-list").appendChild(historyList);
+
+  // Show Modal
+  document.getElementById("success-modal").showModal();
 });
