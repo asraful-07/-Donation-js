bad863cd339af6a12862acec5a38a0c475febbfe
asraful-07 <asraful0508@gmail.com>
2024-09-25 10:38:44 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -19,29 +19,12 @@ document.getElementById("donate-now").addEventListener("click", function () {
   document.getElementById("current-donation").innerText = newDonationTotal.toFixed(2);
 
   // History add
-  const now = new Date();
-  // Format the date to include the day of the week and the date
-  const formattedDate = now.toLocaleDateString("en-BD", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  // Format the time to include hours, minutes, seconds, and time zone
-  const formattedTime = now.toLocaleTimeString("en-BD", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    timeZoneName: "short",
-  });
-
+  const date = new Date();
   const historyList = document.createElement("div");
   historyList.className = "bg-white shadow-xl rounded border-2 p-6 mb-6";
   historyList.innerHTML = `
-    <h3 class="text-xl font-bold my-4">${donationInput} Taka is Donated for famine-2024 at Noakhali, Banglades</h3>
-    <p class="text-lg text-black">Date: ${formattedDate} ${formattedTime} (Bangladesh Standard Time)</p>
-   
+    <h3 class="text-xl font-bold my-4">${donationInput} Taka is Donated for famine-2024 at Noakhali, Bangladesh</h3>
+    <p class="text-lg text-black">Date: ${date}</p>
     `;
   document.getElementById("history-list").appendChild(historyList);
 
@@ -66,4 +49,14 @@ showDonation.addEventListener("click", function () {
   showHistory.classList.remove("text-black", "bg-[#b4f461]");
   document.getElementById("donation-section").classList.remove("hidden");
   document.getElementById("history-section").classList.add("hidden");
+});
+
+
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add('backdrop-blur-md', 'bg-opacity-90');
+  } else {
+    navbar.classList.remove('backdrop-blur-md', 'bg-opacity-90');
+  }
 });
--- js/index3.js
+++ js/index3.js
@@ -22,30 +22,16 @@ document.getElementById("donation-btn").addEventListener("click", function () {
     newDonationTotalAmount.toFixed(2);
 
   // history add
-  const now = new Date();
-  // Format the date to include the day of the week and the date
-  const formattedDate = now.toLocaleDateString("en-BD", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+    const date = new Date();
+    const historyList = document.createElement("div");
+    historyList.className = "bg-white shadow-xl rounded border-2 p-6 mb-6";
+    historyList.innerHTML = `
+      <h3 class="text-xl font-bold my-4">${donatedInputType} Taka is Donated for famine-2024 at Quota Movement, Bangladesh</h3>
+      <p class="text-lg text-black">Date: ${date}</p>
+      `;
+    document.getElementById("history-list").appendChild(historyList);
 
-  // Format the time to include hours, minutes, seconds, and time zone
-  const formattedTime = now.toLocaleTimeString("en-BD", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    timeZoneName: "short",
-  });
-
-  const historyList = document.createElement("div");
-  historyList.className = "bg-white shadow-xl rounded border-2 p-6 mb-6";
-  historyList.innerHTML = `
-    <h3 class="text-xl font-bold my-4">${donatedInputType} Taka is Donated for famine-2024 at Quota Movement, Banglades</h3>
-    <p class="text-lg text-black">Date: ${formattedDate} ${formattedTime} (Bangladesh Standard Time)</p>
-    `;
-  document.getElementById("history-list").appendChild(historyList);
+ 
 
   // Show Modal
   document.getElementById("success-modal").showModal();
